e9b997c8800eba6326995cdd59458418a1bde6e6
mMay03 <yamkumari.thapa03@gmail.com>
2023-02-22 23:47:23 +0545

    again routes

--- src/components/DataNavbar.js
+++ src/components/DataNavbar.js
@@ -1,5 +1,5 @@
 const topNav = [
-  { id: 1, label: "Home", href: "/" },
+  { id: 1, label: "Home", href: "/TravelWebsite/" },
   { id: 2, label: "About", href: "/about" },
   // { id: 3, label: "Skills", href: "#" },
   // { id: 5, label: "Portfolio", href: "#" },
