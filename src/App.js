f9973417be612ea030e45ae8e8a42ddce067bbe4
mMay03 <yamkumari.thapa03@gmail.com>
2023-02-22 23:06:08 +0545

    again fixing routes

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
       <Navbar />
       <Routes>
         {/* <Route path="/" element={<Home />}> */}
-        <Route path="/TravelWebsite" element={<Home />} />
+        <Route exact path="/TravelWebsite" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/contact" element={<Contact />} />
 
