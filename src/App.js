8f12586c80efaec675f76dc9c14fa33fadf3bb99
mMay03 <yamkumari.thapa03@gmail.com>
2023-02-22 23:18:12 +0545

    routings

--- src/App.js
+++ src/App.js
@@ -9,22 +9,13 @@ import "bootstrap/dist/css/bootstrap.min.css";
 // animation
 import AOS from "aos";
 import "aos/dist/aos.css";
+import Router from "./Router";
 AOS.init();
 function App() {
   return (
-    <HashRouter>
-      <Navbar />
-      <Routes>
-        {/* <Route path="/" element={<Home />}> */}
-        <Route exact path="/TravelWebsite" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/contact" element={<Contact />} />
-
-        {/* <Route path="contact" element={<Contact />} />
-          <Route path="*" element={<NoPage />} /> */}
-        {/* </Route> */}
-      </Routes>
-    </HashRouter>
+    <>
+      <Router />
+    </>
   );
 }
 
---
+++ src/Router.js
@@ -0,0 +1,29 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import Home from "./pages/Home";
+
+const Router = () => {
+  return (
+    <>
+      {" "}
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          {/* <Route path="/" element={<Home />}> */}
+          <Route exact path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+
+          {/* <Route path="contact" element={<Contact />} />
+      <Route path="*" element={<NoPage />} /> */}
+          {/* </Route> */}
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
+};
+
+export default Router;
