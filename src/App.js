830971368b1c368ac6cc883fdf0045043771d761
mMay03 <yamkumari.thapa03@gmail.com>
2023-02-22 23:52:08 +0545

    again routes iooooookkkkkkk

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ function App() {
         <Navbar />
         <Routes>
           {/* <Route path="/" element={<Home />}> */}
-          <Route exact path="/TravelWebsite/" element={<Home />} />
+          <Route exact path="/TravelWebsite" element={<Home />} />
           <Route path="/about" element={<About />} />
           <Route path="/contact" element={<Contact />} />
 
