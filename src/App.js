128604b8d6f8a4b7d319bc804f98c35405d0ceb1
mMay03 <yamkumari.thapa03@gmail.com>
2023-02-22 23:32:59 +0545

    routes fixing shitttttt

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import "./App.css";
 // import { BrowserRouter } from "react-router-dom";
-import { HashRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import About from "./pages/About";
 import Home from "./pages/Home";
@@ -14,7 +14,19 @@ AOS.init();
 function App() {
   return (
     <>
-      <RouterComp />
+      <BrowserRouter basename="/TravelWebsite">
+        <Navbar />
+        <Routes>
+          {/* <Route path="/" element={<Home />}> */}
+          <Route exact path="/TravelWebsite" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+
+          {/* <Route path="contact" element={<Contact />} />
+      <Route path="*" element={<NoPage />} /> */}
+          {/* </Route> */}
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
